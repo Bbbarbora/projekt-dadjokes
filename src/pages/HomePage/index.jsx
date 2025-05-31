17719e52e48fbe78fa47e0142e101c4de7137bdc
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-05-31 12:44:35 +0200

    odpoved c.15

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,46 @@
 import './style.css';
+import { useState } from 'react';
 
 export const HomePage = () => {
+  const[likesUp, setLikesUp] = useState(0);
+  const[likesDown, setLikesDown] = useState(0);
+
+  const handleLikeUp = () => {
+    setLikesUp(likesUp + 1);
+  };
+
+  const handleLikeDown = () => {
+    setLikesDown(likesDown + 1);
+  };
+
+
   return (
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+              alt="user avatar"
+            />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+
+        <div className="joke__likes">
+          <button onClick={handleLikeUp} className="btn-like btn-like--up"></button>
+          <span className="likes-count likes-count--up">{likesUp}</span>
+          <button onClick={handleLikeDown} className="btn-like btn-like--down"></button>
+          <span className="likes-count likes-count--down">{likesDown}</span>
+        </div>
+      </div>
     </div>
   );
 };
